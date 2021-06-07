48195fe21c8f399bfb69ee64ee5a9316b1b70bc0
Fabio Vedovelli <vedovelli@gmail.com>
2021-06-07 15:23:51 +0200

    rehersal

--- components/Login.spec.js
+++ components/Login.spec.js
@@ -4,9 +4,13 @@ describe('Login.vue', () => {
   it.todo('should display an input type[password]');
   it.todo('should display a submit button');
   it.todo('should ensure both fields have placeholder text');
-  it.todo('should make a POST call on form submit');
   it.todo('should NOT make a POST call on form submit when form data is not fully filled');
   it.todo('should display feedback when fields are missing');
-  it.todo('should NOT display feedback when form is submitted');
+  it.todo('should make a POST call on form submit');
   it.todo('should clear form fields on submit success');
+  it.todo('should change submit button label to "Please wait..." during submit');
+  it.todo('should change submit button label back to "Log in" after submit');
+  it.todo('should toggle disable submit button during submit');
+  it.todo('should toggle disable both fields during submit');
+  it.todo('should display a message when credentials are not valid');
 });
